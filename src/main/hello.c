16abf26a8b26a5b4dbc1a64aba91bf7c4a08d589
Paolo Bosetti <paolo.bosetti@unitn.it>
2023-03-17 14:49:05 +0100

    Fixes

--- src/main/hello.c
+++ src/main/hello.c
@@ -3,10 +3,8 @@
 
 int main(int argc, char **argv) {
   printf("Hello, world!\n");
-  eprintf("  [MACRO] message %d", 10);
-  wprintf("[MACRO] message");
-  error("  [function] Message no args");
-  error("  [function] Message with args: %d", 10);
-  warning("[function] warning message al line %d", __LINE__);
+  printf("version %s (%s build)\n", VERSION, BUILD_TYPE);
+  eprintf("  [MACRO] message with argument %d", 10);
+  wprintf("[MACRO] message, no argument");
   return 0;
 }
