484a4002e971b80404b2b39ba99fd3974db66dbe
Paolo Bosetti <paolo.bosetti@unitn.it>
2023-03-22 15:05:43 +0100

    Lesson 20220322

---
+++ examples/src/0_hello.c
@@ -0,0 +1,35 @@
+//   _   _      _ _         __        __         _     _ _ 
+//  | | | | ___| | | ___    \ \      / /__  _ __| | __| | |
+//  | |_| |/ _ \ | |/ _ \    \ \ /\ / / _ \| '__| |/ _` | |
+//  |  _  |  __/ | | (_) |    \ V  V / (_) | |  | | (_| |_|
+//  |_| |_|\___|_|_|\___( )    \_/\_/ \___/|_|  |_|\__,_(_)
+//                      |/                                 
+// To create big text as above, in VSC select a line, type Ctrl-Shift-p, type
+// "fig" and then select "FIGlet: create ASCII art text": the selected text will
+// become bigger. Then, without changing the selection, type Ctrl-Shift-7
+// (italian keyboard) and all the selectd lines get commented
+
+#include <stdio.h>
+#define GREET "Hello, World!"
+
+int main() {
+  printf(GREET "\n"); // note *
+  return 0;
+}
+
+// * - This "string" is called a string literal. When two string literals are
+//     placed next to each other (with no commas in between!) the compiler
+//     simply merges them. So "Hello, World!" "\n" becomes "Hello, World!\n".
+
+// now run this command:
+//   clang -E examples/src/1_hello.c | code -
+// -E: means "preprocess only"
+// |: means pass the output (or "pipe") to the next command (code)
+// code -: means open a new file in VSC with the output coming from previous
+// command
+//
+// This will open a temporary file with the source after preprocessor.
+// You may notice the whole stdio.h included at the begining, 
+// also, the GREET preprocessor constant has been replaced in line 12.
+// Lines starting with "# " are not compiled and are tags representing 
+// preprocessor inserion traces
---
+++ examples/src/2_basics.c
@@ -0,0 +1,37 @@
+//   ____            _
+//  | __ )  __ _ ___(_) ___ ___
+//  |  _ \ / _` / __| |/ __/ __|
+//  | |_) | (_| \__ \ | (__\__ \
+//  |____/ \__,_|___/_|\___|___/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const **argv) {
+  // DECLARATIONS
+  // better put them at the beginning of code block 
+  double factor[2];
+  unsigned int i;
+  // declaration and initialization on the same line are OK too
+  double result = 0; // default result code: 0 means "OK!"
+
+  // CODE
+  // check CLI arguments
+  if (argc != 3) {
+    fprintf(stderr, "I need two arguments, got %d\n", argc - 1);
+    result = -1;
+    goto end; // the one and only case where a goto is accepted!
+  }
+  // convert args from strings to numbers
+  for (i = 0; i < 2; i++) {
+    factor[i] = atof(argv[i + 1]);
+  }
+  // multiply the two arguments
+  result = factor[0] * factor[1];
+  // print the result
+  printf("%f * %f = %f\n", factor[0], factor[1], result);
+
+end:
+  // a single exit point
+  return result;
+}
