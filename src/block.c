86250ebd865f52050f84b1001c7b1a7fb6e267b4
Paolo Bosetti <paolo.bosetti@unitn.it>
2023-04-13 14:41:49 +0200

    Added program

--- src/block.c
+++ src/block.c
@@ -486,10 +486,14 @@ int main(int argc, char const *argv[]) {
   {
     data_t t = 0, tq = machine_tq(m), dt = block_dt(b2);
     data_t lambda = 0, v = 0;
-    point_t *pos;
     printf("t lambda v x y z\n");
-    for (t = 0; /*exercise*/; t += tq) {
-      // Exercise
+    for (t = 0; t - dt < tq/10.0; t += tq) {
+      lambda = block_lambda(b2, t, &v);
+      block_interpolate(b2, lambda);
+      printf("%f %f %f %.3f %.3f %.3f\n", t, lambda, v, 
+        point_x(machine_setpoint(m)),
+        point_y(machine_setpoint(m)),
+        point_z(machine_setpoint(m)));
     }
   }
 
