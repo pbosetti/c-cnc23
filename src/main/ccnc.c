c17987de441ae218d96af75ea3ebddd0bf03cc0f
Paolo Bosetti <paolo.bosetti@unitn.it>
2023-05-31 09:20:00 +0200

    settings on RT pacing

--- src/main/ccnc.c
+++ src/main/ccnc.c
@@ -21,7 +21,7 @@ int main(int argc, char const *argv[]) {
   };
   ccnc_state_t cur_state = CCNC_STATE_INIT;
   data_t rt_pacing = machine_rt_pacing(state_data.machine);
-  useconds_t dt = machine_tq(state_data.machine) * 1E6 * rt_pacing;
+  useconds_t dt = machine_tq(state_data.machine) * 1E6 / rt_pacing;
   useconds_t dt_max = dt * 10;
 
   if (!state_data.machine) {
